1e36030456b91e6910ca8ee6a2bca06fe2c2e477
AsparagusEduardo <eduardo602002@gmail.com>
2020-05-01 03:20:51 -0400

    Loads types

--- IKPokeEditor/Form1.cs
+++ IKPokeEditor/Form1.cs
@@ -53,6 +53,7 @@ namespace IKPokeEditor
                 fileName = FBD.FileName;
                 directory = FBD.FileName;
                 bool checkResult = checkFilesExists();
+                //bool checkResult = true;
 
                 if (checkResult == true)
                 {
@@ -63,9 +64,9 @@ namespace IKPokeEditor
                     loadData();
                     guardarToolStripMenuItem.Enabled = true;
 
-                    comboBox1.SelectedIndex = 1;
-                    comboBox2.SelectedIndex = 1;
-                    comboBox3.SelectedIndex = 1;
+                    //comboBox1.SelectedIndex = 1;
+                    //comboBox2.SelectedIndex = 1;
+                    //comboBox3.SelectedIndex = 1;
                 } else
                 {
                     MessageBox.Show("No se ha encontrado el archivo " + fileErr + " en el directorio " + pathErr);
@@ -79,36 +80,36 @@ namespace IKPokeEditor
             var pathToSearch = "";
 
             string[] filePaths = {
-                (directory.ToString() + "\\data\\graphics\\pokemon\\back_pic_table.inc"),
-                (directory.ToString() + "\\data\\graphics\\pokemon\\front_pic_table.inc"),
-                (directory.ToString() + "\\data\\graphics\\pokemon\\back_pic_coords.inc"),
-                (directory.ToString() + "\\data\\graphics\\pokemon\\front_pic_coords.inc"),
-                (directory.ToString() + "\\data\\graphics\\pokemon\\graphics.inc"),
-                (directory.ToString() + "\\data\\graphics\\pokemon\\palette_table.inc"),
-                (directory.ToString() + "\\data\\graphics\\pokemon\\shiny_palette_table.inc"),
+                //(directory.ToString() + "\\data\\graphics\\pokemon\\back_pic_table.inc"),
+                //(directory.ToString() + "\\data\\graphics\\pokemon\\front_pic_table.inc"),
+                //(directory.ToString() + "\\data\\graphics\\pokemon\\back_pic_coords.inc"),
+                //(directory.ToString() + "\\data\\graphics\\pokemon\\front_pic_coords.inc"),
+                //(directory.ToString() + "\\data\\graphics\\pokemon\\graphics.inc"),
+                //(directory.ToString() + "\\data\\graphics\\pokemon\\palette_table.inc"),
+                //(directory.ToString() + "\\data\\graphics\\pokemon\\shiny_palette_table.inc"),
                 (directory.ToString() + "\\include\\constants\\species.h"),
-                (directory.ToString() + "\\include\\global.h"),
-                (directory.ToString() + "\\include\\graphics.h"),
-                (directory.ToString() + "\\include\\pokedex.h"),
-                (directory.ToString() + "\\sound\\direct_sound_data.inc"),
-                (directory.ToString() + "\\sound\\voice_groups.inc"),
-                (directory.ToString() + "\\src\\battle\\battle_1.c"),
-                (directory.ToString() + "\\src\\data\\pokemon\\base_stats.h"),
-                (directory.ToString() + "\\src\\data\\pokemon\\cry_ids.h"),
-                (directory.ToString() + "\\src\\data\\pokemon\\level_up_learnset_pointers.h"),
-                (directory.ToString() + "\\src\\data\\pokemon\\level_up_learnsets.h"),
-                (directory.ToString() + "\\src\\data\\pokemon\\tmhm_learnsets.h"),
-                (directory.ToString() + "\\src\\data\\text\\species_names_en.h"),
-                (directory.ToString() + "\\src\\data\\pokedex_entries_en.h"),
-                (directory.ToString() + "\\src\\data\\pokedex_orders.h"),
-                (directory.ToString() + "\\src\\pokedex.c"),
-                (directory.ToString() + "\\src\\pokemon_1.c"),
-                (directory.ToString() + "\\src\\pokemon_icon.c"),
-                (directory.ToString() + "\\src\\data\\text\\move_names_en.h"),
-                (directory.ToString() + "\\src\\data\\items_en.h"),
-                (directory.ToString() + "\\include\\pokemon.h"),
-                (directory.ToString() + "\\include\\constants\\abilities.h"),
-                (directory.ToString() + "\\src\\data\\pokemon\\evolution.h"),
+                //(directory.ToString() + "\\include\\global.h"),
+                //(directory.ToString() + "\\include\\graphics.h"),
+                //(directory.ToString() + "\\include\\pokedex.h"),
+                //(directory.ToString() + "\\sound\\direct_sound_data.inc"),
+                //(directory.ToString() + "\\sound\\voice_groups.inc"),
+                //(directory.ToString() + "\\src\\battle\\battle_1.c"),
+                //(directory.ToString() + "\\src\\data\\pokemon\\base_stats.h"),
+                //(directory.ToString() + "\\src\\data\\pokemon\\cry_ids.h"),
+                //(directory.ToString() + "\\src\\data\\pokemon\\level_up_learnset_pointers.h"),
+                //(directory.ToString() + "\\src\\data\\pokemon\\level_up_learnsets.h"),
+                //(directory.ToString() + "\\src\\data\\pokemon\\tmhm_learnsets.h"),
+                //(directory.ToString() + "\\src\\data\\text\\species_names_en.h"),
+                //(directory.ToString() + "\\src\\data\\pokedex_entries_en.h"),
+                //(directory.ToString() + "\\src\\data\\pokedex_orders.h"),
+                //(directory.ToString() + "\\src\\pokedex.c"),
+                //(directory.ToString() + "\\src\\pokemon_1.c"),
+                //(directory.ToString() + "\\src\\pokemon_icon.c"),
+                //(directory.ToString() + "\\src\\data\\text\\move_names_en.h"),
+                //(directory.ToString() + "\\src\\data\\items .h"),
+                (directory.ToString() + "\\include\\constants\\pokemon.h"),
+                //(directory.ToString() + "\\include\\constants\\abilities.h"),
+                //(directory.ToString() + "\\src\\data\\pokemon\\evolution.h"),
             };
 
             List<string> folderPathsList = new List<string>();
@@ -117,7 +118,7 @@ namespace IKPokeEditor
                 (directory.ToString() + "\\graphics\\pokemon"),
                 (directory.ToString() + "\\graphics\\pokemon_icon_palettes"),
             };*/
-
+            /*
             folderPathsList.Add(directory.ToString() + "\\graphics\\pokemon");
             folderPathsList.Add(directory.ToString() + "\\graphics\\pokemon_icon_palettes");
 
@@ -128,6 +129,7 @@ namespace IKPokeEditor
             {
                 folderPathsList.Add((directory.ToString() + "\\sound\\direct_sound_samples\\cries"));
             }
+            */
 
             for (int i = 0; i < filePaths.Length; i++)
             {
@@ -173,16 +175,16 @@ namespace IKPokeEditor
 
         private void loadData()
         {
-            loadPokemonBaseStats();
-            loadPokemonNames();
-            loadPokemonEvolutions();
-            loadPokemonMovements();
-            loadPokemonMTMO();
-            loadDexData();
-            loadSpriteData();
-            //addArgumentsData();
-            addMovementsData();
-            addMTMOData();
+            //loadPokemonBaseStats();
+            //loadPokemonNames();
+            //loadPokemonEvolutions();
+            //loadPokemonMovements();
+            //loadPokemonMTMO();
+            //loadDexData();
+            //loadSpriteData();
+            ////addArgumentsData();
+            //addMovementsData();
+            //addMTMOData();
         }
 
         private void loadPokemonNames()
@@ -1498,36 +1500,36 @@ namespace IKPokeEditor
             string str = null;
 
             string[] filePaths = {
-                ("pFile_back_pic_table_inc"),
-                ("pFile_front_pic_table_inc"),
-                ("pFile_back_pic_coords_inc"),
-                ("pFile_front_pic_coords_inc"),
-                ("pFile_graphics_inc"),
-                ("pFile_palette_table_inc"),
-                ("pFile_shiny_palette_table_inc"),
+                //("pFile_back_pic_table_inc"),
+                //("pFile_front_pic_table_inc"),
+                //("pFile_back_pic_coords_inc"),
+                //("pFile_front_pic_coords_inc"),
+                //("pFile_graphics_inc"),
+                //("pFile_palette_table_inc"),
+                //("pFile_shiny_palette_table_inc"),
                 ("pFile_species_h"),
-                ("pFile_global_h"),
-                ("pFile_graphics_h"),
-                ("pFile_pokedex_h"),
-                ("pFile_direct_sound_data_inc"),
-                ("pFile_voice_groups_inc"),
-                ("pFile_battle_1_c"),
-                ("pFile_base_stats_h"),
-                ("pFile_cry_ids_h"),
-                ("pFile_level_up_learnset_pointers_h"),
-                ("pFile_level_up_learnsets_h"),
-                ("pFile_tmhm_learnsets_h"),
-                ("pFile_species_names_en_h"),
-                ("pFile_pokedex_entries_en_h"),
-                ("pFile_pokedex_orders_h"),
-                ("pFile_pokedex_c"),
-                ("pFile_pokemon_1_c"),
-                ("pFile_pokemon_icon_c"),
-                ("pFile_move_names_en_h"),
-                ("pFile_items_en_h"),
+                //("pFile_global_h"),
+                //("pFile_graphics_h"),
+                //("pFile_pokedex_h"),
+                //("pFile_direct_sound_data_inc"),
+                //("pFile_voice_groups_inc"),
+                //("pFile_battle_1_c"),
+                //("pFile_base_stats_h"),
+                //("pFile_cry_ids_h"),
+                //("pFile_level_up_learnset_pointers_h"),
+                //("pFile_level_up_learnsets_h"),
+                //("pFile_tmhm_learnsets_h"),
+                //("pFile_species_names_en_h"),
+                //("pFile_pokedex_entries_en_h"),
+                //("pFile_pokedex_orders_h"),
+                //("pFile_pokedex_c"),
+                //("pFile_pokemon_1_c"),
+                //("pFile_pokemon_icon_c"),
+                //("pFile_move_names_en_h"),
+                //("pFile_items_en_h"),
                 ("pFile_pokemon_h"),
-                ("pFile_abilities_h"),
-                ("pFile_evolution_h")
+                //("pFile_abilities_h"),
+                //("pFile_evolution_h")
             };
 
             for (int i = 0; i < filePaths.Length; i++)
@@ -1555,18 +1557,20 @@ namespace IKPokeEditor
             infoData.Add("metodoEvolutivo", new Dictionary<string, string>());
 
             setTypesData();
-            setMovementsData();
+            /*setMovementsData();
             setItemsData();
             setMTMOData();
             setEggAndColorData();
             setAbilitiesData();
             setGrowthData();
-            setEvolutiveMethodData();
+            setEvolutiveMethodData();*/
         }
 
         private void setTypesData()
         {
             string str = data["pFile_pokemon_h"].ToString();
+            //string str = directory.ToString() + FilePaths.Constants.Pokemon;
+
             int index = 0;
             int lastIndex = 0;
 
