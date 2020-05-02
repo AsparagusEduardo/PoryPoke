65af9fc3e6db46c76ec9a45407576dbe77a25b9c
AsparagusEduardo <eduardo602002@gmail.com>
2020-05-02 03:25:18 -0400

    Changed icon and name.

--- IKPokeEditor/Form1.Designer.cs
+++ IKPokeEditor/Form1.Designer.cs
@@ -1565,7 +1565,7 @@
             this.MaximizeBox = false;
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "IKPokeEditor";
+            this.Text = "PoryPoke";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.menuStrip1.ResumeLayout(false);
             this.menuStrip1.PerformLayout();
